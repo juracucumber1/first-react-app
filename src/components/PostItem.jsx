a2652492489e7e0985f5cc31d90466b5beee8ac9
Jura <mr.gordien@mail.ru>
2021-10-19 21:56:15 +0300

    Revert "fix-post-del"
    This reverts commit 5e7acfa566aa8bab5aa7649376023b25a660e8a2.

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -11,7 +11,7 @@ const PostItem = (props) => {
                 </div>
             </div>
             <div className="post__btns">
-                <MyButton onClick={() => props.remove(props.post)}>
+                <MyButton onCllick={() => props.remove(props.post)}>
                     Удалить
                 </MyButton>
             </div>
