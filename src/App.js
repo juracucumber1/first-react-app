3315b431f6b461f8e8dc615bfbe86b21f90c54d6
Jura <mr.gordien@mail.ru>
2021-10-22 02:44:31 +0300

    add-jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -7,31 +7,25 @@ import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
+import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'w', body: 'апа'},
-        {id: 2, title: 'ASD', body: 'ии'},
-        {id: 3, title: 'sdfsaf 3', body: 'ррр'},
-    ])
+    const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
-
-    const sortedPosts = useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts;
-    }, [filter.sort, posts])
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-    }, [filter.query, sortedPosts])
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
     }
+    
+    async function fetchPosts() {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        console.log(response)
+    }
+    
 // Получаем post из дочернего компонента
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
@@ -42,8 +36,9 @@ function App() {
 
   return (
     <div className="App">
+        <button onClick={fetchPosts}>GET POSTS</button>
         <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-            Создать пользователя
+            Создать пост
         </MyButton>
         <MyModal visible={modal} setVisible={setModal}>
             <PostForm create={createPost}/>
@@ -68,4 +63,6 @@ export default App;
 
 // Тайминг 1:15:00
 // Тайминг 1:22:00
+// Тайминг 1:30:00
+// Тайминг 1:36:00
 
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PostItem from "./PostItem";
+import TransitionGroup from "react-transition-group/cjs/TransitionGroup";
+import CSSTransition from "react-transition-group/cjs/CSSTransition";
 
 const PostList = ({posts, title, remove}) => {
 
@@ -16,9 +18,18 @@ const PostList = ({posts, title, remove}) => {
             <h1 style={{textAlign: 'center'}}>
                 {title}
             </h1>
-            {posts.map((post, index) =>
-                <PostItem remove={remove} number={index + 1} post={post} key={post.id}/>
-            )}
+
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                    <PostItem remove={remove} number={index + 1} post={post} />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 };
