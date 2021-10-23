6b3740b2887ee73d4e2b63f4f3950501406508f0
Jura <mr.gordien@mail.ru>
2021-10-23 03:08:03 +0300

    pagination

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
 import {getPageCount, getPagesArray} from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 
 
 
@@ -28,9 +29,9 @@ function App() {
     const [limit, setLimit] = useState(10);
     const [page, setPage] = useState(1);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-    let pagesArray = getPagesArray(totalPages);
 
-    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
         const response = await PostService.getAll(limit, page)
         setPosts(response.data)
         const totalCount = response.headers['x-total-count']
@@ -39,7 +40,7 @@ function App() {
 
 
     useEffect(() => {
-        fetchPosts()
+        fetchPosts(limit, page)
     }, [])
 
     const createPost = (newPost) => {
@@ -54,7 +55,8 @@ function App() {
 
     const changePage = (page) => {
         setPage(page)
-        fetchPosts()
+        fetchPosts(limit, page)
+
     }
 
 
@@ -79,17 +81,12 @@ function App() {
             ? <div style={{display:'flex', justifyContent: 'center', marginTop: 50}}> <Loader/> </div>
             : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про Что-то"/>
         }
-        <div className="page__wrapper">
-            {pagesArray.map(p =>
-                <span
-                    onClick={ () => changePage}
-                    key={p}
-                    className={page === p ? 'page page__current' : 'page'}
-                >
-                    {p}
-                </span>
-            )}
-        </div>
+        <Pagination
+            page={page} 
+            changePage={changePage}
+            totalPages={totalPages}
+        />
+
 
     </div>
   );
