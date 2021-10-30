31426dd2d2c068024f3743154c1bc41d915d0712
Jura <mr.gordien@mail.ru>
2021-10-30 14:18:44 +0300

    dinam-nawigation
    useHistory, useParams

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -11,7 +11,12 @@ export default class PostService {
                 }
         })
             return response;
-
-
     }
+
+    static async getById(id) {
+
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+        return response;
+    }
+
 }
---
+++ src/pages/PostIdPage.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import {useFetching} from "../hooks/useFetching";
+import Loader from "../components/UI/Loader/Loader";
+import PostService from "../API/PostService";
+
+
+const PostIdPage = () => {
+    const params = useParams()
+    const [post, setPost] = useState({},);
+    const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+        const response = await PostService.getById(id)
+        setPost(response.data);
+    })
+
+    useEffect(() => {
+    fetchPostById(params.id)
+    })
+
+    return (
+        <div>
+            <h1>Вы открыли страницу поста c ID = {params.id}</h1>
+            {isLoading
+                ? <Loader/>
+                : <div> {post.id}. {post.title}</div>
+            }
+        </div>
+    );
+};
+
+export default PostIdPage;
